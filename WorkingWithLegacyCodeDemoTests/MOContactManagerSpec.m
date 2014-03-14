7517fee9e58604375c6c920b4e6f21ecdd56e6e5
Maciej Oczko <maciek.oczko@polidea.com>
2014-03-14 23:27:50 +0100

    Added non-passing test to save model.

--- WorkingWithLegacyCodeDemoTests/MOContactManagerSpec.m
+++ WorkingWithLegacyCodeDemoTests/MOContactManagerSpec.m
@@ -1,21 +1,50 @@
 #import <Kiwi/Kiwi.h>
+#import <PLCoreDataUtils/NSManagedObjectContext+PLCoreDataUtils.h>
 
 #import "MOContactManager.h"
 #import "MOCoreDataStack.h"
 #import "MOSpecHelperCoreDataStack.h"
+#import "MOContactDataGroup.h"
+#import "MOContactData.h"
 
 SPEC_BEGIN(MOContactManagerSpec)
 
 describe(@"MOContactManager", ^{
     __block MOContactManager *manager;
+    __block MOCoreDataStack *coreDataStack;
 
     beforeEach(^{
-        MOCoreDataStack *coreDataStack = [[MOSpecHelperCoreDataStack alloc] init];
-
+        coreDataStack = [[MOSpecHelperCoreDataStack alloc] init];
         manager = [[MOContactManager alloc] initWithCoreDataStack:coreDataStack];
     });
 
     describe(@"saving data to model", ^{
+
+        beforeAll(^{
+
+            NSDictionary *responseObject = @{
+                    @"response" : @[
+                            @{
+                                    @"name" : @"John",
+                                    @"surname" : @"Jackson",
+                                    @"role" : @1,
+                            }
+                    ]
+            };
+
+            [manager saveDataToModel:responseObject];
+
+        });
+
+        it(@"should contain one Contact Data Group object", ^{
+            NSArray *contactGroups = [coreDataStack.mainContext fetchObjectsWithEntityName:NSStringFromClass([MOContactDataGroup class]) predicate:nil];
+            [[contactGroups should] haveCountOf:1];
+        });
+
+        it(@"should contain one Contact Data object", ^{
+            NSArray *contactGroups = [coreDataStack.mainContext fetchObjectsWithEntityName:NSStringFromClass([MOContactData class]) predicate:nil];
+            [[contactGroups should] haveCountOf:1];
+        });
 
     });
 });
